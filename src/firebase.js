2f0c458536c771df93cc93b26d05e0e6d0d6cb53
Carlos Sánchez <djkrlosmix1@gmail.com>
2019-06-11 08:58:20 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import "firebase/storage";
 import "firebase/database";
 
 const config = {
-  apiKey: "AIzaSyABv9ph3c1AocqrZMUd9nv2cCW6j6b5tLk",
-  authDomain: "react-clone-slack-6bc4f.firebaseapp.com",
-  databaseURL: "https://react-clone-slack-6bc4f.firebaseio.com",
-  projectId: "react-clone-slack-6bc4f",
-  storageBucket: "react-clone-slack-6bc4f.appspot.com",
-  messagingSenderId: "1081903252769",
-  appId: "1:1081903252769:web:ff94bb05107ae45d"
+  config: "you config firebase"
 };
 
 firebase.initializeApp(config);
